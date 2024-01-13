acff8106c292394f4a37e71f144e1d499f5d0937
Sumit Mishra <sumit.mishra@uniacco.com>
2024-01-13 19:14:18 +0530

    feat: moved connection details to env

--- database/db.js
+++ database/db.js
@@ -1,14 +1,21 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 const dotenv = require("dotenv").config();
 
-// Replace the following with your PostgreSQL connection details
-const pool = new Pool({
+const devConfig = {
   user: process.env.USER,
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: process.env.DB_PORT, // Usually 5432
-});
+};
+
+const propConfig = {
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? propConfig : devConfig
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
